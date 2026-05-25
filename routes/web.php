e3930183a752dd64a5fcabeaf232d0e007b05b4a
sumit <sumit.hack03@gmail.com>
2026-05-26 00:55:27 +0530

    PM: added seeding data api

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-seed', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+    return "Bhai, Database Seed ho gaya!";
+});
